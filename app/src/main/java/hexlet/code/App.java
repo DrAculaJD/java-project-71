8423c1b86921a5e6dab0e9a64ad235e5d70b6b31
Dracula <ammaddeuss@gmail.com>
2023-03-08 18:52:57 +0400

    created Makefile, added reference "Help" to App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,23 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+
+public class App implements Callable<Integer> {
     public static void main(String[] args) {
+        final int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
         System.out.println("Hello world!");
+        return null;
     }
 }
+
