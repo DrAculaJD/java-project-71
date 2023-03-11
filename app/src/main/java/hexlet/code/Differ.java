1d32ddcf27b3b1241503a992aeeaa83d7550f7e6
Dracula <ammaddeuss@gmail.com>
2023-03-11 23:28:40 +0400

    removed the contents of the source files from the output

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -9,6 +9,11 @@ import java.util.TreeMap;
 
 public class Differ {
 
+    public static String generate(String filepath1, String filepath2) throws Exception {
+        final String defaultFormat = "stylish";
+        return generate(filepath1, filepath2, defaultFormat);
+    }
+
     public static String generate(String filepath1, String filepath2, String format) throws Exception {
 
         final String firstFileAbsolutePath = checkIsFileExistThenToAbsolutePath(filepath1);
